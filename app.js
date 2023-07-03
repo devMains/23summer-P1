6e14b8ec8c1a2cb31da96eb5ebb4537bbe042546
devMains <wltjrdl2@gmail.com>
2023-07-03 15:53:51 +0900

    자유게시판 db연결

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 const path = require('path');
@@ -6,6 +8,19 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static('js'));
 app.use(express.static('img'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const mongoURI = "mongodb+srv://mainsdev:1q2w3e4r!@cluster0.v3mragw.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> console.log('mongodb connected'))
+    .catch(err => console.log(err));
+
+const Post = mongoose.model('post', {
+    title: String,
+    content: String
+})
+
 
 app.get('/', (req, res) => {
     res.sendFile( __dirname + "/htmls/index.html");
@@ -22,6 +37,50 @@ app.get('/login', (req, res) => {
 app.get('/page', (req, res) => {
     res.sendFile(__dirname + "/htmls/page.html");
 });
+
+app.post('/api/posts', (req, res) => {
+    const { title, content } = req.body;
+
+    const newPost = new Post({title, content});
+    newPost.save()
+        .then(posts => res.json(posts))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
+
+app.get('/api/posts', (req, res) => {
+    Post.find({})
+        .then(posts => res.json(posts))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
+
+app.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    Post.findByIdAandUpdate(id, { title, content })
+        .then(()=> res.sendStatus(200))
+        .catch(err => {
+            console.log(err);
+            res.sendStatuse(500);
+        })
+})
+
+app.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+  
+    Post.findOneAndRemove(id)
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  });
 
 
 app.listen(port, () => {
