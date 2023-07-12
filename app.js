39ef77e7ce166d75b28c927e43f746101de1a6cc
devMains <wltjrdl2@gmail.com>
2023-07-12 14:03:21 +0900

    시간 표시

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
 const Post = mongoose.model('post', {
     title: String,
     content: String,
-    replies: [String]
+    replies: [String],
+    postTime: String
 })
 
 
@@ -51,15 +52,24 @@ app.get('/detail', (req, res) => {
 });
 
 app.post('/api/posts', (req, res) => {
-    const { title, content } = req.body;
+    const { title, content, postTime } = req.body;
 
-    const newPost = new Post({title, content});
+    const newPost = new Post({title, content, postTime: getCurrentDateTime()});
     newPost.save()
         .then(posts => res.json(posts))
         .catch(err => {
             console.log(err);
             res.sendStatus(500);
         });
+    function getCurrentDateTime() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = (now.getMonth() + 1).toString().padStart(2, '0');
+        const day = now.getDate().toString().padStart(2, '0');
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');            return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    };
 });
 
 app.get('/api/posts', (req, res) => {
